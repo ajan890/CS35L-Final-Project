932b5667e555367b60250d1e5ce10e2bebe3e7ff
Howard-Z <67174263+Howard-Z@users.noreply.github.com>
2022-10-28 04:00:48 -0700

    Create index.json

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+  );
+  const element = <h1>Hello, world</h1>;
+  root.render(element);
