4884eadc11eff194d080f80491f91530b1625260
ajan890 <adnjan2011@gmail.com>
2022-10-29 15:49:40 -0700

    a

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDocs, getFirestore } from "firebase/firestore";
+import { collection } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDhVImrXhCHZzckmpPC0N4ZPacZKjTc0xI",
+  authDomain: "cs35l-final-project-b0129.firebaseapp.com",
+  databaseURL: "https://cs35l-final-project-b0129-default-rtdb.firebaseio.com",
+  projectId: "cs35l-final-project-b0129",
+  storageBucket: "cs35l-final-project-b0129.appspot.com",
+  messagingSenderId: "265891179928",
+  appId: "1:265891179928:web:642ee13badcbbd6f300fed",
+  measurementId: "G-KBKX6H2ZL6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+const querySnapshot = await getDocs(collection(db, "Users"));
+querySnapshot.forEach((doc) => {
+  console.log(doc.data());
+});
+
+
