d8357a3691d4df7b3bb496957edcc91a2a93a6af
Justin Hu <justinoxhu@gmail.com>
2022-11-17 03:40:36 -0800

    Top of Page + Scrolling
    Fit 2 background images with the bottom one sliding over the top one as inspired by Wix, some polishing up

--- src/home.js
+++ src/home.js
@@ -4,17 +4,21 @@ function Home()
 {
     return (
         <html>
+        <img class="background_one" src={require("./pen_and_notebook.jpg")} alt="Something's Wrong!"></img>
 
+        <div class="top_overlay">
+            <div class="title_text">A CS35L Project</div>
+        </div>
+        
         <form action={'/login'}>
-            <button class="login" type="submit">Login</button>
+            <button class="login_button" type="submit">Login</button>
         </form>
 
         <a href={'/requests'}>Requests</a>
 
         <a href={'/'}>Home</a>
 
-        <div>This is Home</div>
-        <img class="background" src={require("./royce_hall.jpg")} alt="You done fucked up kid"></img>
+        <img class="background_two" src={require("./royce_hall.jpg")} alt="You done fucked up kid"></img>
         </html>
     );
 }
