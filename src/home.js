ad0bbe6417a674e4679bf74ff749f855105cf8cd
Justin Hu <justinoxhu@gmail.com>
2022-11-17 15:44:33 -0800

    1/2 Features List, savepoint
    Will attempt to go back and redo the CSS using relative positioning instead of absolute or else the webpage will only look nice on a 16:9 monitor (Not even sure if it'll work with different resolutions)

--- src/home.js
+++ src/home.js
@@ -3,29 +3,57 @@ import "./index.js"
 function Home()
 {
     return (
+        
         <html>
-        <img class="background_one" src={require("./pen_and_notebook.jpg")} alt="Something's Wrong!"></img>
 
-        <div class="top_overlay">
-            <div class="title_text">A CS35L Project</div>
+        <img id="background_one" src={require("./pen_and_notebook.jpg")} alt="Something's Wrong!"></img> 
+
+        <div id="top_overlay">
+            <div id="title_text">A CS35L Project</div>
         </div>
         
-        <div class="login_title">Join the Network</div>
-        <div class="login_description">
+
+
+        <img id="background_two" src={require("./royce_hall.jpg")} alt="You done fucked up kid"></img>
+
+        <div id="login_title">Join the Network</div>
+        <div id="login_description">
         For those small requests that just aren't worth your time, your fellow bruins got your back.<br></br>        
         Or perhaps lend a helping hand, and help bring joy to the community</div>
 
         <form action={'/login'}>
-            <button class="login_button" type="submit">Login</button>
+            <button id="login_button" type="submit">Login</button>
         </form>
 
-        <a href={'/requests'}>Requests</a>
 
-        <a href={'/'}>Home</a>
+        <div id="Services">Our Services</div>
 
-        <img class="background_two" src={require("./royce_hall.jpg")} alt="You done fucked up kid"></img>
+        <img id="background_three" src={require("./snowy_mountain.jpg")} alt="Where the mountains at"></img>
+        
+        <div id="services_container">
+            <div class="service_element">Bruh
+                <div class="service_text"><br></br>
+                My ass is nice
+                </div>
+            </div>
+            <div class="service_element">Bruh
+                <div class="service_text"><br></br>
+                Some services
+                </div>
+            </div>
+            <div class="service_element">Bruh
+                <div class="service_text"><br></br>
+                Not bad
+                </div>
+            </div>
+        </div>
+        
         </html>
     );
 }
+
+//<a href={'/requests'}>Requests</a>
+
+//<a href={'/'}>Home</a>
 
 export default Home
