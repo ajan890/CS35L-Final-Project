b6151d2a19345c1eaa7e897fb9a57d7d352eda03
Justin Hu <justinoxhu@gmail.com>
2022-11-17 22:55:07 -0800

    Sections 1-4 Finished, 5 left
    Home page mostly looks good, looking to add one last element at the bottom.

--- src/home.js
+++ src/home.js
@@ -26,25 +26,82 @@ function Home()
         </form>
 
 
-        <div id="Services">Our Services</div>
+
+        <div id="Features">Our Features</div>
+
+
 
         <img id="background_three" src={require("./snowy_mountain.jpg")} alt="Where the mountains at"></img>
         
-        <div id="services_container">
-            <div class="service_element">Bruh
-                <div class="service_text"><br></br>
-                My ass is nice
+        <div id="features_container">
+            <div class="feature_element">
+                <div class="feature_number">&nbsp;&nbsp;&nbsp;1</div>
+                <div class="feature_title">
+                    Make your own<br></br>
+                    Requests
+                </div>
+                <div class="feature_text"><br></br><br></br>
+                    Missed an item on your last<br></br>
+                    shopping trip due to ADHD<br></br> 
+                    or don't feel like leaving<br></br> 
+                    the dorm because of crippling<br></br> 
+                    anxiety and depression? Submit<br></br> 
+                    a request in 10 quick seconds<br></br> 
+                    and have the neurotypicals go<br></br> 
+                    do it for you!
                 </div>
             </div>
-            <div class="service_element">Bruh
-                <div class="service_text"><br></br>
-                Some services
+            <div class="feature_element">
+                <div class="feature_number">&nbsp;&nbsp;&nbsp;2</div>
+                <div class="feature_title">
+                    Fulfill Requests<br></br>
+                    from Others
+                </div>
+                <div class="feature_text"><br></br><br></br>
+                    We know you just love being<br></br>
+                    in the sunlight, and getting<br></br> 
+                    fit. You're already taking 15k<br></br> 
+                    steps a day, why not help a<br></br> 
+                    fellow Bruin out along the way?<br></br> 
+                    Browse requests from the<br></br> 
+                    Dashboard and earn some easy<br></br>
+                    cash!
                 </div>
             </div>
-            <div class="service_element">Bruh
-                <div class="service_text"><br></br>
-                Not bad
+            <div class="feature_element">
+                <div class="feature_number">&nbsp;&nbsp;&nbsp;3</div>
+                <div class="feature_title">
+                    Go get that<br></br>
+                    Bounty
                 </div>
+                <div class="feature_text"><br></br><br></br>
+                    Got a lil competitive spirit<br></br>
+                    in ya? Need some extra money<br></br> 
+                    outside of the three jobs you're<br></br> 
+                    already working to afford UCLA<br></br> 
+                    tuition? Worry not! Our bounty<br></br> 
+                    system makes sure to reward<br></br> 
+                    consistent fulfillers who help<br></br> 
+                    keep the system going.
+                </div>
+            </div>
+        </div>
+
+
+
+        <div id="propoganda">
+            <div id="propoganda_title">
+                From Bruins, to Bruins
+            </div>
+            <div id="propoganda_slogan"><br></br>
+                Better than USC
+            </div>
+            <div id="propoganda_text"><br></br>
+                We came up with this idea totally on the fly since we needed something for our CS35L project but it actually ended<br></br>
+                up sounding like a pretty great community connection thing that would never work in real life due to the amount of<br></br> 
+                trust each individual would have to have towards every other individual. But hey, that's what dreams (and CS class<br></br> 
+                projects) are for right. With the amount of walking around you have to do everyday, why not help a fellow Bruin out<br></br> 
+                by picking up a couple of things along the way?
             </div>
         </div>
         
