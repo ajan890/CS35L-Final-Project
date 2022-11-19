e0ddb770eab8063f6a92f62cc5e18357c0f75d1a
ajan890 <adnjan2011@gmail.com>
2022-11-19 04:20:49 -0800

    Refactored Request.js
    Moved code to create a new request to its own page

--- src/createRequest.js
+++ src/createRequest.js
@@ -1,9 +1,137 @@
 import React from "react"
+import { initializeApp } from "firebase/app";
+import { getDocs, getFirestore } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { Timestamp } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDhVImrXhCHZzckmpPC0N4ZPacZKjTc0xI",
+    authDomain: "cs35l-final-project-b0129.firebaseapp.com",
+    databaseURL: "https://cs35l-final-project-b0129-default-rtdb.firebaseio.com",
+    projectId: "cs35l-final-project-b0129",
+    storageBucket: "cs35l-final-project-b0129.appspot.com",
+    messagingSenderId: "265891179928",
+    appId: "1:265891179928:web:642ee13badcbbd6f300fed",
+    measurementId: "G-KBKX6H2ZL6"
+  };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const labelStyle = { color: 'red', };  
+
+function onClickCreateRequest() {
+    var sendRequest = true;
+    var name = document.getElementById('name_textbox').value
+    if (name == "") {
+      sendRequest = false;
+      document.getElementById('name_req_label').innerText = '* Required';
+    }
+    var desc = document.getElementById('desc_textbox').value
+    if (desc == "") {
+      sendRequest = false;
+      document.getElementById('desc_req_label').innerText = '* Required';
+    }
+    var tagsString = document.getElementById('tags_textbox').value
+    if (tagsString == "") {
+      sendRequest = false;
+      document.getElementById('tags_req_label').innerText = '* Required';
+    } else {
+      var tags = tagsString.split(',');
+    }
+    var bounty = document.getElementById('bounty_textbox').value
+    if (bounty == "") {
+      sendRequest = false;
+      document.getElementById('bounty_req_label').innerText = '* Required';
+    }
+    var loc = document.getElementById('from_textbox').value
+    //loc is not required.  If for example a client wants a roll of paper towels, they may not care where it came from
+    var dest = document.getElementById('to_textbox').value
+    if (dest == "") {
+      sendRequest = false;
+      document.getElementById('dest_req_label').innerText = '* Required';
+    }
+  
+    if (sendRequest) {
+      addRequest(name, desc, tags, bounty, getAuth().currentUser.uid, loc, dest);
+      console.log(name);
+      console.log(desc);
+      console.log(tags);
+      document.getElementById('name_req_label').innerText = '';
+      document.getElementById('desc_req_label').innerText = '';
+      document.getElementById('tags_req_label').innerText = '';
+      document.getElementById('bounty_req_label').innerText = '';
+      document.getElementById('dest_req_label').innerText = '';
+      document.getElementById('field').style.color = "#00FF00";
+      document.getElementById('field').innerText = 'Request Submitted! 😃';
+    } else {
+      document.getElementById('field').style.color = "#FF0000";
+      document.getElementById('field').innerText = 'Please check your input.😭';
+    }
+  
+    //print request submitted
+  
+  }
+
+function addRequest(name, desc, tagsArray, bounty1, userID, from, to) {
+    //name, description = strings, tags = string[]
+    setDoc(doc(db, "Requests", (+new Date).toString(36)), {
+      status: "Not Taken", //can either be "Not Taken," "Taken," "Filled"
+      time_submitted: Timestamp.now(),
+      fulfill_pin: Math.floor((Math.random() * 9000) + 1000),
+      title: name,
+      description: desc,
+      from: from,
+      destination: to,
+      bounty: bounty1,
+      tags: tagsArray,
+      user: userID
+    });
+  }
+
 class CreateRequest extends React.Component
 {
     render(){
+        const handleSubmit = event => {
+            event.preventDefault();
+            onClickCreateRequest();
+          }
         return(
-            <h1>This is the new requests page</h1>
+            <div className="wrapper">
+                <h1>This is the new requests page</h1>
+                <form onSubmit={handleSubmit}>
+                <label>
+                <div>
+                    <a>Name: </a>
+                    <input type="text" id="name_textbox"/> <label id="name_req_label" style={labelStyle}/>
+                </div>
+                <div>
+                    <a>Description: </a>
+                    <input type="text" id="desc_textbox"/> <label id="desc_req_label" style={labelStyle}/>
+                </div>
+                <div>
+                    <a>Tags (separate with commas): </a>
+                    <input type="text" id="tags_textbox"/> <label id="tags_req_label" style={labelStyle}/>
+                </div>
+                <div>
+                    <a>Bounty: </a>
+                    <input type="text" id="bounty_textbox"/> <label id="bounty_req_label" style={labelStyle}/>
+                </div>
+                <div>
+                    <a>From (Which store or location do you want your product from?): </a>
+                    <input type="text" id="from_textbox"/>
+                </div>
+                <div>
+                    <a>Destination: </a>
+                    <input type="text" id="to_textbox"/> <label id="dest_req_label" style={labelStyle}/>
+                </div>
+                </label>
+                <button type="submit">Submit</button>
+                <div>
+                <p id="field"/>
+                </div>
+            </form>
+            </div>
         );
     }
 }
--- src/requests.js
+++ src/requests.js
@@ -1,10 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getDocs, getFirestore } from "firebase/firestore";
 import { collection } from "firebase/firestore";
-import { doc, setDoc } from "firebase/firestore";
 import { useEffectOnce } from "./singleEffect.js";
 import { getAuth } from "firebase/auth";
-import { Timestamp } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,10 +18,6 @@ const firebaseConfig = {
   messagingSenderId: "265891179928",
   appId: "1:265891179928:web:642ee13badcbbd6f300fed",
   measurementId: "G-KBKX6H2ZL6"
-};
-
-const labelStyle = {
-  color: 'red',
 };
 
 // Initialize Firebase
@@ -70,123 +64,18 @@ function printRequests(querySnapshot) {
 
 }
 
-function addRequest(name, desc, tagsArray, bounty1, userID, from, to) {
-  //name, description = strings, tags = string[]
-  setDoc(doc(db, "Requests", (+new Date).toString(36)), {
-    status: "Not Taken", //can either be "Not Taken," "Taken," "Filled"
-    time_submitted: Timestamp.now(),
-    fulfill_pin: Math.floor((Math.random() * 9000) + 1000),
-    title: name,
-    description: desc,
-    from: from,
-    destination: to,
-    bounty: bounty1,
-    tags: tagsArray,
-    user: userID
-  });
-}
-
-function onClick() {
-  var sendRequest = true;
-  var name = document.getElementById('name_textbox').value
-  if (name == "") {
-    sendRequest = false;
-    document.getElementById('name_req_label').innerText = '* Required';
-  }
-  var desc = document.getElementById('desc_textbox').value
-  if (desc == "") {
-    sendRequest = false;
-    document.getElementById('desc_req_label').innerText = '* Required';
-  }
-  var tagsString = document.getElementById('tags_textbox').value
-  if (tagsString == "") {
-    sendRequest = false;
-    document.getElementById('tags_req_label').innerText = '* Required';
-  } else {
-    var tags = tagsString.split(',');
-  }
-  var bounty = document.getElementById('bounty_textbox').value
-  if (bounty == "") {
-    sendRequest = false;
-    document.getElementById('bounty_req_label').innerText = '* Required';
-  }
-  var loc = document.getElementById('from_textbox').value
-  //loc is not required.  If for example a client wants a roll of paper towels, they may not care where it came from
-  var dest = document.getElementById('to_textbox').value
-  if (dest == "") {
-    sendRequest = false;
-    document.getElementById('dest_req_label').innerText = '* Required';
-  }
-
-  if (sendRequest) {
-    addRequest(name, desc, tags, bounty, getAuth().currentUser.uid, loc, dest);
-    console.log(name);
-    console.log(desc);
-    console.log(tags);
-    document.getElementById('name_req_label').innerText = '';
-    document.getElementById('desc_req_label').innerText = '';
-    document.getElementById('tags_req_label').innerText = '';
-    document.getElementById('bounty_req_label').innerText = '';
-    document.getElementById('dest_req_label').innerText = '';
-    document.getElementById('field').style.color = "#00FF00";
-    document.getElementById('field').innerText = 'Request Submitted! 😃';
-  } else {
-    document.getElementById('field').style.color = "#FF0000";
-    document.getElementById('field').innerText = 'Please check your input.😭';
-  }
-
-  //print request submitted
-
-}
-
 function testform()
 {
-  const handleSubmit = event => {
-    event.preventDefault();
-    onClick();
-  }
   return(
     <div className="wrapper">
       <h1>Requests</h1> 
           <div id="requests" className="scrollmenu"></div>
       <h1>My Requests</h1>
         <div id="myRequests" className="scrollmenu"></div>
-      <h1>Submit Request</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <div>
-            <a>Name: </a>
-            <input type="text" id="name_textbox"/> <label id="name_req_label" style={labelStyle}/>
-          </div>
-          <div>
-            <a>Description: </a>
-            <input type="text" id="desc_textbox"/> <label id="desc_req_label" style={labelStyle}/>
-          </div>
-          <div>
-            <a>Tags (separate with commas): </a>
-            <input type="text" id="tags_textbox"/> <label id="tags_req_label" style={labelStyle}/>
-          </div>
-          <div>
-            <a>Bounty: </a>
-            <input type="text" id="bounty_textbox"/> <label id="bounty_req_label" style={labelStyle}/>
-          </div>
-          <div>
-            <a>From (Which store or location do you want your product from?): </a>
-            <input type="text" id="from_textbox"/>
-          </div>
-          <div>
-            <a>Destination: </a>
-            <input type="text" id="to_textbox"/> <label id="dest_req_label" style={labelStyle}/>
-          </div>
-        </label>
-        <button type="submit">Submit</button>
-        <div>
-          <p id="field"/>
-        </div>
-      </form>
-      <div>
+        <a href="./newrequest">
+          <button>Submit Request</button>
+        </a>
         
-      </div>
     </div>
   )
 }
