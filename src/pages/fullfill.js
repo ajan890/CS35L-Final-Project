8d4306d71cd31889247518765d01c3af93cdc37a
Yuelong Zhang <zylupupup@hotmail.com>
2022-11-26 13:45:49 -0800

    wierd result

--- src/pages/fullfill.js
+++ src/pages/fullfill.js
@@ -3,7 +3,6 @@ import { doc, collection } from "firebase/firestore";
 import { useEffectOnce } from "../utilities.js";
 import { db, auth } from "../firebase/initFirebase.js"
 
-var requests_taken; //person who wants to the order
 var user;      // person who fullfills the order
 
 //query
@@ -18,6 +17,10 @@ async function getRequests() {
   printRequests(querySnapshot);
 }
 
+function getRequestTake(user) {
+   return user.requests_taken;
+}
+
 async function getUser() {
   const users = await getDocs(collection(db, "Users"));
   const authUID = auth.currentUser.uid;
@@ -26,24 +29,38 @@ async function getUser() {
     console.log(userIter.data().UID);
     if (userIter.data().UID === authUID) {
       user = userIter.data();
+      
     }
   });
   console.log("1USER: " + user.UID);
+  
 }
 
-function onClickTakeReq(request) {
-  console.log("Taking request");
+function onClickTakeReq(request,form) {
+  console.log("Checking pin");
+  console.log(form.value);
   var id = request.id;
   console.log(request);
   console.log("Request ID: " + id + " User: " + user.UID);
   updateDoc(doc(db, "Requests", id), {
-    status: "Taken"
+    status: "Fullfilled"
   });
   var newRequests = user.requests_taken;
-  newRequests.push(id)
+
+  var remove_idx = -1;
+  for (var i = 0; i < newRequests.length; ++i) {
+    if (newRequests[i] === id) {
+        remove_idx = i;
+        break;
+    }
+  }
+  console.log("remove idx is:" + remove_idx);
+  newRequests = newRequests.splice(remove_idx, remove_idx + 1);
   updateDoc(doc(db, "Users", user.UID), {
     requests_taken: newRequests
   });
+
+  //getRequests();
 
   //user.requests_taken.push(id);
 }
@@ -56,30 +73,36 @@ function formatRequest(request) {
     var title = document.createElement('h2');
     var desc = document.createElement('p');
     var tags = document.createElement('p');
-    var form = document.createElement('form');
+    var form = document.createElement('input');
+    form.value = "Enter 6 digits pin";
+    
     //start of button
     var btn = document.createElement("button");
     btn.textContent = "Fullfill Order"
-    btn.onclick = () => onClickTakeReq(data);
+    btn.onclick = () => onClickTakeReq(request, form);
     title.innerText = data.title;
     desc.innerText = data.description;
     tags.innerText = data.tags;
     temp.appendChild(title);
     temp.appendChild(desc);
     temp.appendChild(tags);
-    temp.appendChild(btn);
     temp.appendChild(form);
+    temp.appendChild(btn);
+    
 
   return temp;
 }
 
 function printRequests(querySnapshot) {
-  querySnapshot.forEach((request) => {
+  querySnapshot.forEach((request, Users) => {
     //document.getElementById('requests').appendChild(formatRequest(request));
     console.log("Request User: " + request.data().user);
     console.log("Current User Login: " + auth.currentUser.uid);
-    if (request.data().user === auth.currentUser.uid) {
-      document.getElementById('myRequests').appendChild(formatRequest(request));
+   
+    var requests_taken = getRequestTake(auth.currentUser);
+    console.log(requests_taken);
+    if (requests_taken.include(request.data().id)) {
+       document.getElementById('myTakenRequests').appendChild(formatRequest(request));
     }
   });
 
@@ -90,7 +113,7 @@ function testform()
   return(
     <div className="wrapper">
       <h1>My Requests</h1>
-        <div id="myRequests" className="scrollmenu"></div>
+        <div id="myTakenRequests" className="scrollmenu"></div>
     </div>
   )
 }
